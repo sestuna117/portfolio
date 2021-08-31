6a71ef6c60675dfd8e424b3c32566f35db63ab88
JGuo <jason.guo2560@gmail.com>
2021-08-31 17:35:07 -0400

    v0.2.7 - added collage to home page for me appeal

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -3,23 +3,31 @@ import { IoIosArrowForward } from "react-icons/all";
 import { NavLink } from "react-router-dom";
 import "./HomePage.css";
 import Links from "../Links";
+import Collage from "../../imgs/pictures/collage.png";
 
 export default function HomePage() {
   return (
     <div className="home-page">
       <div className="content-page">
-        <div className={"home-text"}>
-          <div className={"home-intro"}>Hi, my name is Shio Chio.</div>
-          <p className={"home-title"}>Aspiring Web Developer</p>
-          <p className={"home-status"}>
-            Currently a university student at Stony Brook University working
-            towards Bachelors Degree in Computer Science.
-          </p>
-          <NavLink className="nav-link" to="/about" activeClassName="selected">
-            <span className="learn-more">
-              More about me <IoIosArrowForward className="learn-more-arrow" />
-            </span>
-          </NavLink>
+        <div className="home-content">
+          <div className={"home-text"}>
+            <p className={"home-intro"}>Hi, my name is Shio Chio.</p>
+            <p className={"home-title"}>Aspiring Web Developer</p>
+            <p className={"home-status"}>
+              Currently a university student at Stony Brook University working
+              towards Bachelors Degree in Computer Science.
+            </p>
+            <NavLink
+              className="nav-link"
+              to="/about"
+              activeClassName="selected"
+            >
+              <span className="learn-more">
+                More about me <IoIosArrowForward className="learn-more-arrow" />
+              </span>
+            </NavLink>
+          </div>
+          <img className="home-images" src={Collage} alt={"artwork_prev"} />
         </div>
         <Links />
       </div>
